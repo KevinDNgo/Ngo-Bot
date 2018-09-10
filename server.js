7312c5d49f87456b2d98fa54d5a19bb92e764609
Kevin <kngo225@mail.fresnostate.edu>
2018-09-09 17:52:01 -0700

    Added userInfo function
    Allows users to obtain account ID and creation date

--- server.js
+++ server.js
@@ -26,10 +26,22 @@ app.get("/", (request, response) => {
   response.sendStatus(200);
 });
 
-//app.listen(3000);
+// app.listen(3000);
 setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
+
+// Returns concatenated string with user information
+function userInfo(user) {
+    var finalString = '';
+
+    finalString += '**' + user.username + '**, with ID **' + user.id + '**';
+
+    var userCreated = user.createdAt.toString().split(' ');
+    finalString += ', was **created on ' + userCreated[1] + ' ' + userCreated[2] + ', ' + userCreated[3] + '**.'
+
+    return finalString;
+}
 
 // Confirms client is working on developer end
 client.on('ready', () => {
@@ -38,6 +50,24 @@ client.on('ready', () => {
 
 // Checks if message is music bot command or response, and deletes it
 client.on('message', message => {
+  
+    var sender = message.author;
+    var msg = message.content.toUpperCase();
+    var prefix = '>>'
+    
+    // Checks if user is bot, and ignores message
+    if (sender.id === '488242523240660993' || sender.id === '184405311681986560' || sender.id === '204777316621090816') {
+        return;
+    }
+  
+    // Runs userInfo function if user enters '>>' command
+    if (msg.startsWith(prefix + 'USERINFO')) {
+        if (msg === prefix + 'USERINFO') {
+            message.channel.send(userInfo(sender));
+        }
+    }
+  
+    // Removes commands and bot messages from main channel
     if(message.channel.id === '133104742254510080') {
         if(message.author.bot) {
             message.delete(60000)
@@ -47,8 +77,12 @@ client.on('message', message => {
             message.delete(60000)
             .catch(console.error);
         }
+        if(message.content.startsWith('>>')) {
+            message.delete(60000)
+            .catch(console.error);
+        }
     }
 });
 
-// Processes private token from .env file
+// Process private token
 client.login(process.env.TOKEN);

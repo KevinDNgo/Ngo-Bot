b06b09f1fca97d777668fb836652b71e34f7ff06
Kevin <kngo225@mail.fresnostate.edu>
2018-09-09 19:37:49 -0700

    Commented and cleaned up code

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const express = require('express');
 const app = express();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const guild = client.guilds.get('133104742254510080');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -38,7 +39,7 @@ function userInfo(user) {
     finalString += '**' + user.username + '**, with ID **' + user.id + '**';
 
     var userCreated = user.createdAt.toString().split(' ');
-    finalString += ', was **created on ' + userCreated[1] + ' ' + userCreated[2] + ', ' + userCreated[3] + '**.'
+    finalString += ', was **created on ' + userCreated[1] + ' ' + userCreated[2] + ', ' + userCreated[3] + '**. \n'
 
     return finalString;
 }
@@ -47,37 +48,37 @@ function userInfo(user) {
 client.on('ready', () => {
     console.log('Ready!');
 });
-
+      
 // Checks if message is music bot command or response, and deletes it
 client.on('message', message => {
   
+    // Variables for parsing user information
     var sender = message.author;
     var msg = message.content.toUpperCase();
     var prefix = '>>'
     
     // Checks if user is bot, and ignores message
     if (sender.id === '488242523240660993' || sender.id === '184405311681986560' || sender.id === '204777316621090816') {
-        return;
+      return;
     }
-  
+    
     // Runs userInfo function if user enters '>>' command
     if (msg.startsWith(prefix + 'USERINFO')) {
-        if (msg === prefix + 'USERINFO') {
-            message.channel.send(userInfo(sender));
-        }
-    }
-  
+        message.channel.send(userInfo(sender))
+        .catch(console.error);
+    }  
+                          
     // Removes commands and bot messages from main channel
-    if(message.channel.id === '133104742254510080') {
-        if(message.author.bot) {
+    if (message.channel.id === '133104742254510080') {
+        if (message.author.bot) {
             message.delete(60000)
             .catch(console.error);
         }
-        if(message.content.startsWith(';;')) {
+        if (message.content.startsWith(';;')) {
             message.delete(60000)
             .catch(console.error);
         }
-        if(message.content.startsWith('>>')) {
+        if (message.content.startsWith('>>')) {
             message.delete(60000)
             .catch(console.error);
         }
